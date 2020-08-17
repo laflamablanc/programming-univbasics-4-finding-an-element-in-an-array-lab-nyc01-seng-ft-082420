b1ba8cadf024f7deaf3c35e5559b6645c4bbfa0e
Sean LaFlam <sean.laflam@gmail.com>
2020-08-17 17:28:58 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,10 @@
 def find_element_index(array, value_to_find)
-  
+  index = 0 
+  while index < array.length 
+    if array[index] == value_to_find
+      return index
+    else
+      index += 1 
+    end
+  end
 end
